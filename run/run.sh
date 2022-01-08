57870f25b917b45f40e2a13345c63ad7cfd58c79
lahari07 <laharichepuri@gmail.com>
2022-01-08 12:54:25 -0500

    Add files via upload

---
+++ run/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# file that gets run when the docker container starts up
+# Sam Fryer.
+
+#first, start the nodejs server to receive any initial commands
+node run/run.js $1 &
+
+#then wait 10 seconds to make sure the commands came and executed
+sleep 10
+
+#finally, start the initial program
+java camera $1 $2
